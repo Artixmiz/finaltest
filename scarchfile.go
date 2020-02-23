eb8f5ca9f03f5747df61dced4511ecd9bcf78c2f
eak0546 <57831268+eak0546@users.noreply.github.com>
2020-02-23 10:20:12 +0700

    Update scarchfile.go

--- scarchfile.go
+++ scarchfile.go
@@ -1,5 +1,15 @@
-package finaltest
+package main
 
-func main() {
+import "os"
 
+func getDrives() (r []string) {
+	for _, drive := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+		f, err := os.Open(string(drive) + ":\\")
+		if err == nil {
+			d := string(drive) + ":/"
+			r = append(r, d)
+			f.Close()
+		}
+	}
+	return
 }
